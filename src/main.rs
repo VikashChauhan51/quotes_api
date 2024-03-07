76282e45c9eb6e5141f8e1c5a16d97134e05eee3
Vikash Chauhan <vikashchauhan51@gmail.com>
2024-03-07 19:16:42 +0530

    configure logger

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
 mod app;
+use log::{info, trace,LevelFilter};
+use env_logger::Builder;
+
 fn main() {
+    Builder::new()
+        .filter(None, LevelFilter::Info)
+        .init();
+
+    trace!("quotes starting...");
+    info!("quotes starting...");
     println!("Hello, world!");
+    info!("done.")
 }
