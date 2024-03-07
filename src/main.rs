f6deb8c67a453787956699e3aa4a505f72bdf82c
Vikash Chauhan <vikashchauhan51@gmail.com>
2024-03-07 20:40:11 +0530

    init server

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,29 @@
 mod app;
 use log::{info, trace,LevelFilter};
 use env_logger::Builder;
+use actix_web::{web, App, HttpServer, Responder};
 
-fn main() {
+async fn index() -> impl Responder {
+    "Hello, world!"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+
+    // init logger
     Builder::new()
         .filter(None, LevelFilter::Info)
         .init();
 
     trace!("quotes starting...");
     info!("quotes starting...");
-    println!("Hello, world!");
-    info!("done.")
+
+    // init server
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await   
 }
