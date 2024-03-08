e5bdff4ee78ee218cf06ea56914a02ca1c2eb354
Vikash Chauhan <vikashchauhan51@gmail.com>
2024-03-08 12:02:57 +0530

    use api route instead of web

--- src/app/handlers.rs
+++ src/app/handlers.rs
@@ -1,6 +1,11 @@
-use actix_web::Responder;
+use actix_web::{get, HttpResponse, Responder};
 use log::info;
+
+#[get("/api/v1/index")]
 pub async fn index() -> impl Responder {
     info!("index called.");
-    "Hello, world!"
+
+    HttpResponse::Ok()
+        .content_type("application/json")
+        .json("{'data':'Hello, world!'}")
 }
--- src/app/routes.rs
+++ src/app/routes.rs
@@ -2,5 +2,5 @@ use actix_web::web;
 use crate::app::handlers;   
 
 pub fn configure(cfg: &mut web::ServiceConfig) {
-    cfg.route("/", web::get().to(handlers::index));
+    cfg.service(handlers::index);
 }
