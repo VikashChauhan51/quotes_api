f7bbc8a938f29661c8afe0ed8865a18827d32d5e
Vikash Chauhan <vikashchauhan51@gmail.com>
2024-03-08 14:37:11 +0530

    enable auth with swagger ui

--- src/app/apidoc.rs
+++ src/app/apidoc.rs
@@ -2,7 +2,10 @@
 use utoipa::OpenApi;
 use crate::app::handlers;
 use crate::models;
-
+use utoipa::{
+    openapi::security::{HttpAuthScheme, HttpBuilder, SecurityScheme},
+    Modify,
+};
 // api doc and handlers and models configuration.
 #[derive(OpenApi)]
     #[openapi(
@@ -14,5 +17,27 @@ use crate::models;
                 models::response::Response //schema models
             ),
         ),
+        modifiers(&SecurityModifier)
     )]
     pub struct ApiDoc;
+
+    //set auth
+    pub struct SecurityModifier;
+    impl Modify for SecurityModifier {
+        fn modify(&self, openapi: &mut utoipa::openapi::OpenApi) {
+            let components = openapi.components.as_mut().unwrap();
+            components.add_security_scheme(
+                "bearer_auth",
+                SecurityScheme::Http(
+                    HttpBuilder::new()
+                        .scheme(HttpAuthScheme::Bearer)
+                        .bearer_format("JWT")
+                        .build(),
+                ),
+            );
+            components.add_security_scheme(
+                "basic_auth",
+                SecurityScheme::Http(HttpBuilder::new().scheme(HttpAuthScheme::Basic).build()),
+            );
+        }
+    }
